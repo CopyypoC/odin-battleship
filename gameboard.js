847dbee55d705bf4ea7b76e693182cc50df90c60
CopyypoC <william.wang650@gmail.com>
2025-04-25 02:05:43 -0700

    feat: add Gameboard class and createBoard() method

---
+++ gameboard.js
@@ -0,0 +1,18 @@
+export class Gameboard {
+  constructor() {
+    this.board = this.createBoard();
+  }
+
+  createBoard() {
+    const board = [];
+    for (let row = 0; row < 10; row++) {
+      const rowArr = [];
+      for (let col = 0; col < 10; col++) {
+        rowArr.push(col);
+      }
+      board.push(rowArr);
+    }
+
+    return board;
+  }
+}
