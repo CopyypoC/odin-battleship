48382937e9481c54f25b1a4ca92c67543f05b727
CopyypoC <william.wang650@gmail.com>
2025-04-27 04:14:02 -0700

    feat: add randomize button UI toggle

--- src/scripts/ui/events.js
+++ src/scripts/ui/events.js
@@ -5,11 +5,14 @@ export function setupHandlers() {
 
 const startBtn = document.querySelector(".start-btn");
 const resetBtn = document.querySelector(".reset-btn");
+const randomizeBtn = document.querySelector(".randomize-btn");
 function handleStart() {
+  // cpuGameboard is dynamically generated, must be in function
   const cpuGameboard = document.querySelector(".cpu-gameboard");
 
   startBtn.addEventListener("click", () => {
     startBtn.disabled = true;
+    randomizeBtn.disabled = true;
     resetBtn.disabled = false;
     cpuGameboard.style.pointerEvents = "auto";
   });
@@ -21,6 +24,7 @@ function handleReset() {
   resetBtn.addEventListener("click", () => {
     resetBtn.disabled = true;
     startBtn.disabled = false;
+    randomizeBtn.disabled = false;
     cpuGameboard.style.pointerEvents = "none";
   });
 }
